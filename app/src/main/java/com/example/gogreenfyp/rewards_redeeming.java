c1a7c5389b65f8eeca5ff237c76079bf9b98f0bf
NGZIBIN <50009798+NGZIBIN@users.noreply.github.com>
2020-07-04 17:10:37 +0800

    Rewards UI

---
+++ app/src/main/java/com/example/gogreenfyp/rewards_redeeming.java
@@ -0,0 +1,14 @@
+package com.example.gogreenfyp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class rewards_redeeming extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rewards_redeeming);
+    }
+}

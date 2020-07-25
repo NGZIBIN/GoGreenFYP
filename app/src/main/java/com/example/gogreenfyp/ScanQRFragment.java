1696d9224d5b6d2c485c52223ea14e7fd5f08994
Bryan Poh <zandrex00@gmail.com>
2020-07-25 15:59:50 +0800

    Added QR Scanner & QR generator for user.

--- app/src/main/java/com/example/gogreenfyp/ScanQRFragment.java
+++ app/src/main/java/com/example/gogreenfyp/ScanQRFragment.java
@@ -1,6 +1,7 @@
 package com.example.gogreenfyp;
 
 import android.Manifest;
+import android.app.Dialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -9,6 +10,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,6 +18,7 @@ import com.budiyev.android.codescanner.CodeScanner;
 import com.budiyev.android.codescanner.CodeScannerView;
 import com.budiyev.android.codescanner.DecodeCallback;
 import com.google.zxing.Result;
+import com.google.zxing.integration.android.IntentIntegrator;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
@@ -31,7 +34,11 @@ public class ScanQRFragment extends Fragment {
 
     CodeScanner codeScanner;
     CodeScannerView scannerView;
-    TextView walletAddress;
+    TextView walletAddress, tv_receiver, tv_amount;
+    Button btn_dialog_yes, btn_dialog_no;
+    Dialog dialog;
+
+    String DIALOG_RESULT;
 
     public ScanQRFragment() {
         // Required empty public constructor
@@ -46,6 +53,7 @@ public class ScanQRFragment extends Fragment {
 
         scannerView = view.findViewById(R.id.scanner_view);
         codeScanner = new CodeScanner(getContext(), scannerView);
+        walletAddress = view.findViewById(R.id.qr_textview);
 
         codeScanner.setDecodeCallback(new DecodeCallback() {
             @Override
@@ -53,26 +61,65 @@ public class ScanQRFragment extends Fragment {
                 getActivity().runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        walletAddress.setText(result.getText());
+                        DIALOG_RESULT = result.getText();
+
+                        dialog = new Dialog(getActivity());
+                        dialog.setContentView(R.layout.dialog_payment_confirmation);
+                        dialog.setCancelable(false);
+                        dialog.show();
+
+                        tv_amount = dialog.findViewById(R.id.tv_payment_amount);
+                        tv_receiver = dialog.findViewById(R.id.tv_payment_title_reciever);
+                        btn_dialog_yes = dialog.findViewById(R.id.btnYes);
+                        btn_dialog_no = dialog.findViewById(R.id.btnNo);
+
+                        // TODO: Split the string to get MERCHANT and AMOUNT
+
+                        // Set merchant
+                        tv_receiver.setText(DIALOG_RESULT);
+
+                        // Set amount
+                        //tv_amount.setText();
+
+                        btn_dialog_no.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                // Dismiss dialog and start scanner again
+                                dialog.dismiss();
+                                codeScanner.startPreview();
+                            }
+                        });
                     }
                 });
             }
         });
-//        scannerView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                codeScanner.startPreview();
-//            }
-//        });
+
+        /*dialog.setContentView(R.layout.dialog_payment_confirmation);
+        dialog.setCancelable(false);
+        dialog.show();*/
+
         return view;
     }
 
     @Override
     public void onResume() {
         super.onResume();
+
+//        requestScanner();
         codeScanner.startPreview();
         requestForCamera();
     }
+
+/*    private void requestScanner(){
+        IntentIntegrator integrator = new IntentIntegrator(getActivity());
+        integrator.setOrientationLocked(false);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+        integrator.setPrompt("Scan QR");
+        integrator.setCameraId(0);  // Use a specific camera of the device
+        integrator.setBeepEnabled(true);
+        integrator.setBarcodeImageEnabled(true);
+        integrator.initiateScan();
+    }*/
 
     private void requestForCamera() {
         Dexter.withContext(getContext()).withPermission(Manifest.permission.CAMERA).withListener(new PermissionListener() {
@@ -83,7 +130,7 @@ public class ScanQRFragment extends Fragment {
 
             @Override
             public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
-//                Toast.makeText(this, "Canera Permission Required", Toast.LENGTH_LONG).show();
+                Toast.makeText(getContext(), "Canera Permission Required", Toast.LENGTH_LONG).show();
             }
 
             @Override
